98474933d58183e53031b9b3a813e4af461bd94c
Iryna Artymuk <artimyk@ukr.net>
2023-04-03 01:05:09 +0300

    add movie details css

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -3,7 +3,14 @@ import { useState, useEffect, Suspense } from 'react';
 
 import { GetMoviesDetails } from '../Service/MovieApi';
 
-import { MovieCard, InfoSection, MovieHeader } from './StyledMovieDetails';
+import {
+  MovieCard,
+  InfoSection,
+  MovieHeader,
+  Genres,
+  MovieDesc,
+  BackgroundImg,
+} from './StyledMovieDetails';
 const MovieDetails = () => {
   const [movieInfo, setMovieInfo] = useState({});
   const [error, setError] = useState(null);
@@ -28,61 +35,54 @@ const MovieDetails = () => {
 
   const Img_url = 'https://image.tmdb.org/t/p/w500';
   console.log(movieInfo);
-  const { title, overview, runtime, release_date } = movieInfo;
+  const { title, overview, runtime, release_date, genres } = movieInfo;
   return (
     <>
       {error && <p>Something went wrong Try again later.😭</p>}
 
       {!error && (
         <>
-          <Link to={backLinkHref}> Go Back </Link>
           <MovieCard id="bright">
             <InfoSection>
+              <Link to={backLinkHref}> Go Back </Link>
               <MovieHeader>
                 <img src={Img_url + movieInfo.poster_path} alt={title} />
                 <h1>{title}</h1>
                 <h4>{release_date}</h4>
-                <span class="minutes">{runtime} min</span>
-                {/* <ul class="type">
-                  {genres.map(item => (
-                    <li>{item.name}</li>
+                <span>{runtime} min</span>
+                <Genres>
+                  {genres?.map((item, index) => (
+                    <li key={index}>{item.name}</li>
                   ))}
-                </ul> */}
+                </Genres>
               </MovieHeader>
-              <div class="movie_desc">
-                <p class="text">{overview}</p>
-              </div>
-              <div class="movie_social">
-                <ul>
-                  <li>
-                    <i class="material-icons">share</i>
-                  </li>
-                  <li>
-                    <i class="material-icons"></i>
-                  </li>
-                  <li>
-                    <i class="material-icons">chat_bubble</i>
-                  </li>
-                </ul>
+              <MovieDesc>
+                <p>{overview}</p>
+              </MovieDesc>
 
-                <ul>
-                  <li>
-                    <Link to="cast" state={{ from: backLinkHref }}>
-                      Cast
-                    </Link>
-                  </li>
-                  <li>
-                    <Link to="reviews" state={{ from: backLinkHref }}>
-                      Reviews
-                    </Link>
-                  </li>
-                </ul>
-              </div>
+              <ul>
+                <li>
+                  <Link to="cast" state={{ from: backLinkHref }}>
+                    Cast
+                  </Link>
+                </li>
+                <li>
+                  <Link to="reviews" state={{ from: backLinkHref }}>
+                    Reviews
+                  </Link>
+                </li>
+              </ul>
+            </InfoSection>
+            <BackgroundImg
+              url={Img_url + movieInfo.poster_path}
+            ></BackgroundImg>
+          </MovieCard>
+          <MovieCard>
+            <InfoSection>
               <Suspense fallback={<div>Loading2...</div>}>
                 <Outlet />
               </Suspense>
             </InfoSection>
-            <div class="blur_back bright_back"></div>
           </MovieCard>
 
           {/* <img src={Img_url + movieInfo.poster_path} alt="" width={200} /> */}
--- src/components/MovieDetails/StyledMovieDetails.jsx
+++ src/components/MovieDetails/StyledMovieDetails.jsx
@@ -5,8 +5,9 @@ const MovieCard = styled.div`
   position: relative;
   display: block;
   width: 800px;
-  height: 350px;
-  margin: 80px auto;
+  height: fit-content;
+
+  margin: 30px auto;
   overflow: hidden;
   border-radius: 10px;
   transition: all 0.4s;
@@ -16,11 +17,27 @@ const MovieCard = styled.div`
     box-shadow: 0px 0px 80px -25px rgba(0, 0, 0, 0.5);
     transition: all 0.4s;
   }
+
+  @media screen and (max-width: 768px) {
+    width: 95%;
+    margin: 30px auto;
+    min-height: 350px;
+    height: auto;
+  }
 `;
 
 const InfoSection = styled.div`
   position: relative;
   width: 100%;
+  @media screen and (min-width: 768px) {
+    background: linear-gradient(to right, #e5e6e6 50%, transparent 100%);
+  }
+
+  @media screen and (max-width: 767px) {
+    background: linear-gradient(to top, #e5e6e6 50%, transparent 100%);
+    display: inline-grid;
+  }
+
   height: 100%;
   background-blend-mode: multiply;
   z-index: 2;
@@ -31,6 +48,13 @@ const MovieHeader = styled.div`
   position: relative;
   padding: 25px;
   height: 40%;
+  @media screen and (min-width: 768px) {
+    width: 65%;
+  }
+  @media screen and (max-width: 768px) {
+    width: 100%;
+    margin-top: 85px;
+  }
   h1 {
     color: black;
     font-weight: 400;
@@ -43,127 +67,72 @@ const MovieHeader = styled.div`
     position: relative;
     float: left;
     margin-right: 20px;
-    height: 120px;
+    height: 100px;
     box-shadow: 0 0 20px -10px rgba(0, 0, 0, 0.5);
+    span {
+      display: inline-block;
+      margin-top: 15px;
+      color: #555;
+      padding: 5px;
+      border-radius: 5px;
+      border: 1px solid rgba(0, 0, 0, 0.05);
+    }
   }
 `;
-//   .info_section{
-//     position: relative;
-//     width: 100%;
-//     height: 100%;
-// 	  background-blend-mode: multiply;
-//     z-index: 2;
-//     border-radius: 10px;
 
-//     .movie_header{
-//   position: relative;
-//   padding: 25px;
-//   height: 40%;
-//   h1{
-//     color: black;
-//     font-weight: 400;
-//   }
-//   h4{
-//     color: #555;
-//     font-weight: 400;
-//   }
-//       .minutes{
-//         display: inline-block;
-//         margin-top: 15px;
-//         color: #555;
-//         padding: 5px;
-//         border-radius: 5px;
-//         border: 1px solid rgba(0,0,0,0.05);
-//       }
-//       .type{
-//         display: inline-block;
-//         color: #959595;
-//         margin-left: 10px;
-//       }
-//
-//     }
-//     .movie_desc{
-//       padding: 25px;
-//       height: 50%;
-//       .text{
-//         color: #545454;
-//       }
-//     }
-//     .movie_social{
-//       height: 10%;
-//       padding-left: 15px;
-//       padding-bottom: 20px;
-//       ul{
-//         list-style: none;
-//         padding: 0;
-//         li{
-//           display: inline-block;
-//           color: rgba(0,0,0,0.3);
-//           transition: color 0.3s;
-//           transition-delay: 0.15s;
-//           margin: 0 10px;
-//           &:hover{
-//             transition: color 0.3s;
-//             color: rgba(0,0,0,0.7);
-//           }
-//           i{
-//             font-size: 19px;
-//             cursor: pointer;
-//           }
-//         }
-//       }
-//     }
-//   }
-//   .blur_back{
-//     position: absolute;
-//     top: 0;
-//     z-index: 1;
-//     height: 100%; right: 0;
-//     background-size: cover;
-//     border-radius: 11px;
-//   }
-// }
+const Genres = styled.ul`
+  display: flex;
 
-// @media screen and (min-width: 768px) {
-//   .movie_header{
-//     width: 65%;
-//   }
+  flex-wrap: wrap;
 
-//   .movie_desc{
-//     width: 50%;
-//   }
+  gap: 10px;
+  color: #959595;
+`;
 
-//   .info_section{
-//     background: linear-gradient(to right, #e5e6e6 50%, transparent 100%);
-//   }
+const MovieDesc = styled.div`
+  padding: 25px;
+  height: 50%;
+  @media screen and (min-width: 768px) {
+    width: 50%;
+  }
+  @media screen and (max-width: 768px) {
+    width: 100%;
+  }
+  p {
+    color: #545454;
+  }
+`;
 
-//   .blur_back{
-//     width: 80%;
-//     background-position: -100% 10% !important;
-//   }
-// }
+const BackgroundImg = styled.div`
+  position: absolute;
+  top: 0;
+  right: 0;
+  z-index: 1;
+
+  height: 100%;
+
+  background-size: cover;
+
+  border-radius: 11px;
+  background: url(${props => props.url});
+  @media screen and (max-width: 768px) {
+    width: 100%;
+    background-position: -100% 10%;
+  }
+  @media screen and (min-width: 768px) {
+    width: 50%;
+    background-position: 50% 50%;
+  }
+
+  height: 100%;
+
+  background-size: contain;
+  background-repeat: no-repeat;
+  border-radius: 11px;
+  background: url(${props => props.url});
+`;
 
 // @media screen and (max-width: 768px) {
-//   .movie_card{
-//     width: 95%;
-//     margin: 70px auto;
-//     min-height: 350px;
-//     height: auto;
-//   }
-
-//   .blur_back{
-//     width: 100%;
-//     background-position: 50% 50% !important;
-//   }
-
-//   .movie_header{
-//     width: 100%;
-//     margin-top: 85px;
-//   }
-
-//   .movie_desc{
-//     width: 100%;
-//   }
 
 //   .info_section{
 //     background: linear-gradient(to top, #e5e6e6 50%, transparent 100%);
@@ -175,4 +144,11 @@ const MovieHeader = styled.div`
 //   background: url("https://occ-0-2433-448.1.nflxso.net/art/cd5c9/3e192edf2027c536e25bb5d3b6ac93ced77cd5c9.jpg");
 // }
 
-export { MovieCard, InfoSection, MovieHeader };
+export {
+  MovieCard,
+  InfoSection,
+  MovieHeader,
+  Genres,
+  MovieDesc,
+  BackgroundImg,
+};
